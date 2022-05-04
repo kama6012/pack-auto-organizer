828299395fa24c6d78fc8926f4bb8c02d7797ac2
kama6012 <kyle.saito2003@gmail.com>
2022-05-04 19:27:19 +0900

    順番変更

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const fs = require('fs')
+const crypto = require('crypto')
+const shasum = crypto.createHash('sha1')
 const fetch = require('cross-fetch')
 const buffer = require("buffer");
 globalThis.Blob = buffer.Blob;
@@ -10,34 +12,36 @@ const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     let zip = JSZip();
-    fetch('https://crafatar.com/skins/565d64224b444734a22eca9ac6039ceb')
-    .then(response => response.buffer())
-    .then(buffer => buffer.toString('base64'))
-    .then(skin =>{
-        zip.file("assets/minecraft/textures/skin.png", skin, {base64: true});
-    }).then(dev => {
-        fetch('https://api.github.com/repos/kama6012/texturepack/contents/MyPack/MyPack.zip?ref=main',{
-          headers: {
-            authorization: '${{ secrets.apikey }}',
-            accept: "application/vnd.github.v3+json"
-          }
+    fetch('https://api.github.com/repos/kama6012/texturepack/contents/MyPack/MyPack.zip?ref=main',{
+      headers: {
+        authorization: '${{ secrets.apikey }}',
+        accept: "application/vnd.github.v3+json"
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      let content = JSON.stringify(data['content'])
+      content = content.replace(/\"/g, "").replace(/\\n/g, "");
+      zip.loadAsync(content, {base64: true})
+      .then(dev => {
+        fetch('https://crafatar.com/skins/565d64224b444734a22eca9ac6039ceb')
+        .then(response => response.buffer())
+        .then(buffer => buffer.toString('base64'))
+        .then(skin =>{
+            zip.remove("assets/minecraft/textures/skin.png")
+            zip.file("assets/minecraft/textures/skin.png", skin, {base64: true})
         })
-        .then(response => response.json())
-        .then(data => {
-          let content = JSON.stringify(data['content'])
-          content = content.replace(/\"/g, "").replace(/\\n/g, "");
-          zip.loadAsync(content, {base64: true})
-          .then(dev => {
-            zip.generateAsync({type:"blob"}).then(content => {
-                console.log(content)
-                res.type(content.type)
-                content.arrayBuffer().then((buf) => {
-                    res.send(Buffer.from(buf))
-                })
+        .then(dev=>{
+          zip.generateAsync({type:"blob"}).then(content => {
+            res.type(content.type)
+            content.arrayBuffer().then((buf) => {
+              shasum.update(Buffer.from(buf));
+              let hash = shasum.digest('hex');
+              res.send(Buffer.from(buf));
             })
           })
         })
-        .catch(error => console.error('通信に失敗しました', error));
+      })
     })
 })
 
