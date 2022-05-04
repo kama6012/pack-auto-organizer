348eca439d11437a75776d6d2389017a49c4c5fa
kama6012 <kyle.saito2003@gmail.com>
2022-05-05 00:05:07 +0900

    req param

--- index.js
+++ index.js
@@ -11,50 +11,46 @@ JSZip.support.blob = true;
 const app = express()
 const port = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-		const UUID = req.query.UUID
-		const mode = req.query.mode
-		console.log(req.query.mode)
-		if (mode == "gen") {
-			console.log("gen mode")
-			let zip = JSZip();
-			fetch('https://api.github.com/repos/kama6012/texturepack/contents/MyPack/MyPack.zip?ref=main',{
-				headers: {
-					authorization: '${{ secrets.apikey }}',
-					accept: "application/vnd.github.v3+json"
-				}
-			})
-			.then(response => response.json())
-			.then(data => {
-				let content = JSON.stringify(data['content'])
-				content = content.replace(/\"/g, "").replace(/\\n/g, "");
-				zip.loadAsync(content, {base64: true})
-				.then(dev => {
-					fetch('https://crafatar.com/skins/565d64224b444734a22eca9ac6039ceb')
-					.then(response => response.buffer())
-					.then(buffer => buffer.toString('base64'))
-					.then(skin =>{
-							zip.remove("assets/minecraft/textures/skin.png")
-							zip.file("assets/minecraft/textures/skin.png", skin, {base64: true})
-					})
-					.then(dev=>{
-						zip.generateAsync({type:"arraybuffer"}).then(content => {
-							fs.mkdir('/tmp', { recursive: true }, (err) => {
-								if (err) throw err;
-							});
-							fs.writeFileSync("/tmp/" + "565d64224b444734a22eca9ac6039ceb" + ".zip", Buffer.from(content))
-							const hash = crypto.createHash('sha1').update(new Uint8Array(content)).digest('hex');
-							res.send(hash);
-						})
+app.get('/gen/:UUID', (req, res) => {
+		let zip = JSZip();
+		fetch('https://api.github.com/repos/kama6012/texturepack/contents/MyPack/MyPack.zip?ref=main',{
+			headers: {
+				authorization: '${{ secrets.apikey }}',
+				accept: "application/vnd.github.v3+json"
+			}
+		})
+		.then(response => response.json())
+		.then(data => {
+			let content = JSON.stringify(data['content'])
+			content = content.replace(/\"/g, "").replace(/\\n/g, "");
+			zip.loadAsync(content, {base64: true})
+			.then(dev => {
+				fetch('https://crafatar.com/skins/565d64224b444734a22eca9ac6039ceb')
+				.then(response => response.buffer())
+				.then(buffer => buffer.toString('base64'))
+				.then(skin =>{
+						zip.remove("assets/minecraft/textures/skin.png")
+						zip.file("assets/minecraft/textures/skin.png", skin, {base64: true})
+				})
+				.then(dev=>{
+					zip.generateAsync({type:"arraybuffer"}).then(content => {
+						fs.mkdir('/tmp', { recursive: true }, (err) => {
+							if (err) throw err;
+						});
+						fs.writeFileSync("/tmp/" + "565d64224b444734a22eca9ac6039ceb" + ".zip", Buffer.from(content))
+						const hash = crypto.createHash('sha1').update(new Uint8Array(content)).digest('hex');
+						res.send(hash);
 					})
 				})
 			})
-		} else if (mode == "send"){
-			res.type("application/zip")
-			res.sendFile("/tmp/565d64224b444734a22eca9ac6039ceb.zip")
-		}
+		})
 	})
 
+app.get('/send/:UUID', (req, res) => {
+	console.log("send pack of " + req.params.UUID)
+	res.type("application/zip")
+	res.sendFile("/tmp/565d64224b444734a22eca9ac6039ceb.zip")
+})
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
 })
